8a51627c65d0c844640c6b71f7de8e1b7712a909
stranak <stranak@ufal.mff.cuni.cz>
2021-02-27 23:03:16 +0100

    loop over the results

--- zoteroToRIV.pl
+++ zoteroToRIV.pl
@@ -16,36 +16,39 @@ use Path::Tiny;
 #GetOptions( "stdout|s" => \our $use_stdout, "out|o" => \our $out_filename )
 #  or die "Usage: $0 [--stdout|-s] [--out|-o] <filename>\n";
 
-my $in_file = 'biblio-export-csl.json';
+my $in_filename = 'biblio-export-csl.json';
+my $out_filename = 'publikace.xml';
 
 # Publikace z CSL JSON souboru ze Zotera
 my $json = JSON->new->allow_nonref;
-my $all_of_it = path( $in_file )->slurp;
+my $all_of_it = path( $in_filename )->slurp;
 my $zotero = $json->decode( $all_of_it );
 #my $zotero = $json->decode( $string );
 say "počet importovaných publikací: ", $#{$zotero};
-my $title = $zotero->[0]->{"title"};
-say $title; 
 
-# TODO: loop over all authors
-my $author_last = $zotero->[0]->{"author"}->[0]->{"family"};
-my $author_given = $zotero->[0]->{"author"}->[0]->{"given"};
-my $author = "$author_given "."$author_last";
-
-# výstup do RIV XML
+# příprava výstupu do RIV XML
 my $encoding = 'UTF-8';
 my $el_name = "result";
-my ($attr_name, $attr_value) = ('author', $author);
 
 my $doc = XML::LibXML::Document->new('1.0',$encoding);
 my $root = $doc->createElementNS( "", "results" );
 $doc->setDocumentElement( $root );
 
-my $element = $doc->createElement($el_name);
-$element->setAttribute( $attr_name, $attr_value );
-$element->appendText( $title );
-$element = $root->appendChild( $element );
+# pro každý výsledek
+for my $res_idx ( 0..$#{$zotero} ) {
+    my $title = $zotero->[$res_idx]->{"title"};
 
+    # TODO: loop over all authors
+    my $author_last = $zotero->[$res_idx]->{"author"}->[0]->{"family"};
+    my $author_given = $zotero->[$res_idx]->{"author"}->[0]->{"given"};
+    my $author = "$author_given "."$author_last";
+
+    my ($attr_name, $attr_value) = ('author', $author);
+    my $element = $doc->createElement($el_name);
+    $element->setAttribute( $attr_name, $attr_value );
+    $element->appendText( $title );
+    $element = $root->appendChild( $element );
+}
 
 #$state = $doc->toFile($filename, $format);
 
@@ -55,5 +58,5 @@ $element = $root->appendChild( $element );
 #else {
     #$doc->setCompression('6');
     #$doc->toFile( $out_filename, 0 );
-    $doc->toFile( 'publikace.xml', 2 );
+    $doc->toFile( $out_filename, 2 );
     #}
